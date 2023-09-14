5379b6933610e88980a7b93bc84757c2a0db4615
achingbrain <alex@achingbrain.net>
2023-09-14 13:20:23 +0100

    chore: update linting and imports

--- packages/interop/test/fixtures/create-helia.ts
+++ packages/interop/test/fixtures/create-helia.ts
@@ -2,7 +2,7 @@ import { kadDHT } from '@libp2p/kad-dht'
 import { createHelia as createNode, type HeliaInit } from 'helia'
 import { identifyService } from 'libp2p/identify'
 import type { Helia } from '@helia/interface'
-import type { Libp2p } from '@libp2p/interface-libp2p'
+import type { Libp2p } from '@libp2p/interface'
 import type { KadDHT } from '@libp2p/kad-dht'
 
 export async function createHelia (init?: Partial<HeliaInit>): Promise<Helia<Libp2p<{ dht: KadDHT, identify: unknown }>>> {
