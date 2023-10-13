1dd9e7019b0f1d647ac663090c7d9690665a33e7
Henrique Dias <hacdias@gmail.com>
2023-10-13 18:08:07 +0200

    feat: PUT ipns

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,4 +1,5 @@
 import getIpnsV1 from './routing/v1/ipns/get.js'
+import putIpnsV1 from './routing/v1/ipns/put.js'
 import getPeersV1 from './routing/v1/peers/get.js'
 import getProvidersV1 from './routing/v1/providers/get.js'
 import type { Helia } from '@helia/interface'
@@ -8,4 +9,5 @@ export default function routes (fastify: FastifyInstance, helia: Helia): void {
   getProvidersV1(fastify, helia)
   getPeersV1(fastify, helia)
   getIpnsV1(fastify, helia)
+  putIpnsV1(fastify, helia)
 }
